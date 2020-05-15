379e99ee50324ae568341a8acc24cc79de1831ba
Ashley Thompson <ashleythompsonbca@gmail.com>
2020-05-15 20:37:34 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,7 +1,8 @@
 # don't forget to add: require 'pry'
+require 'pry'
 
 def snake_it_up(my_string)
-  if my_string[0] == "s"
+  if "my_string[0]" == "s"
   return 10 * "s" + "my_string"
   else
     my_string
