bd98fa4e190e16c8ccd875322c05c6e55f6c8024
Ashley Thompson <ashleythompsonbca@gmail.com>
2020-05-15 21:27:12 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -1,10 +1,7 @@
 # don't forget to add: require 'pry'
-require 'pry'
 
-def generate_star_date
-  (rand(100000) + 400000) / 10.0
-  star_date= (rand(100000)+ 400000) / 10.0
-  binding.pry
+def star_date
+  star_date = (rand(100000) + 400000) / 10.0
 end
 
 
@@ -12,6 +9,11 @@ def state_log(star_date)
   "Captain's Log, star date #{star_date}."
 end
 
-def engage
-  puts state_log(star_date)
+def crew
+  puts "Hello, crew!"
 end
+
+def engage
+  puts crew
+  puts state_log(star_date)
+  end
