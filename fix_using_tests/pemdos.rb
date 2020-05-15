97b2df2cfec032da37a17701e09ca996bb2457d9
Ashley Thompson <ashleythompsonbca@gmail.com>
2020-05-15 20:47:34 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,10 +1,9 @@
 # don't forget to add: require 'pry'
-require 'pry'
 
 def snake_it_up(my_string)
   if "my_string[0]" == "s"
-  return  "s" * "10" + "my_string"
-  else my_string
-  binding.pry
+  return  " " * "10" + "my_string"
+  else 
+    return my_string
   end
 end
